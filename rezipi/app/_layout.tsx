b90690d2146f70cea86f7fff89ae42aec95712f8
Alexandre Boving <alexandre.boving@gmail.com>
2025-03-24 19:38:49 +0100

    expo-camera setup

--- rezipi/app/_layout.tsx
+++ rezipi/app/_layout.tsx
@@ -1,9 +1,28 @@
-import * as React from "react";
 import { StyleSheet } from "react-native";
 import { Tabs, TabList, TabTrigger, TabSlot } from "expo-router/ui";
 import { CustomTabList } from "@/components/CustomTabList";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Tabs>
       <TabSlot />
@@ -14,7 +33,7 @@ export default function Layout() {
       </TabList>
     </Tabs>
 
-    // Hide the TabList component as it's only role is to define the tabs.
+    // Hide the TabList component as it's only role is to define the tabs. The CustomTabList component will be used to render the tabs.
   );
 }
 
